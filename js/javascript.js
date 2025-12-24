55c31f6d52ea5c34f590785117d0e0e77d294b01
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-24 10:23:15 +0630

    Add hover event to change div's background color

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,15 @@
 const container = document.querySelector(".container");
 
 for(let i = 1; i <= 256; i++) {
-    const newDiv = document.createElement('div');
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("hover")
     container.appendChild(newDiv);
 }
+
+const hovers = document.querySelectorAll(".hover");
+
+hovers.forEach(hover => {
+    hover.addEventListener("mouseenter", function(e) {
+        e.target.style.backgroundColor = "white";
+    });
+})
