e4438733cd057e3bf351cd6a099369671b38b266
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-25 10:29:48 +0630

    Add event listener in button to trigger a prompt with max value 100

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,17 @@ const button = document.createElement("button");
 button.textContent = "Create new grid";
 body.insertBefore(button, container);
 
+button.addEventListener("click", function() {
+    const userInput = prompt("Enter a number for grid pair, Max: 100");
+    let gridPair = 0;
+    if(userInput > 100) {
+        alert("Numbers greater than 100 aren't allowed");
+    } else {
+        gridPair = userInput;
+    }
+    console.log(gridPair);
+})
+
 for(let i = 1; i <= 256; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("hover")
